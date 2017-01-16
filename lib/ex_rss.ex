b1b65922ea401ccd01039138f70af06ca77baa33
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-01-16 21:11:39 +0100

    Add crawler to supervision tree

--- lib/ex_rss.ex
+++ lib/ex_rss.ex
@@ -1,11 +1,11 @@
 defmodule ExRss do
   use Application
 
+  import Supervisor.Spec
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
-
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
@@ -14,7 +14,7 @@ defmodule ExRss do
       supervisor(ExRss.Endpoint, []),
       # Start your own worker by calling: ExRss.Worker.start_link(arg1, arg2, arg3)
       # worker(ExRss.Worker, [arg1, arg2, arg3]),
-    ]
+    ] ++ workers(Application.get_env(:ex_rss, :start_crawler))
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
@@ -28,4 +28,7 @@ defmodule ExRss do
     ExRss.Endpoint.config_change(changed, removed)
     :ok
   end
+
+  defp workers(true), do: [ worker(ExRss.Crawler.Queue, []) ]
+  defp workers(_), do: []
 end
---
+++ lib/mix/tasks/ex_rss.server.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.ExRss.Server do
+  use Mix.Task
+
+  @shortdoc "Starts an ExRss server"
+
+  def run(args) do
+    Application.put_env(:ex_rss, :start_crawler, true, persistent: true)
+
+    Mix.Task.run "phoenix.server", args
+  end
+end
