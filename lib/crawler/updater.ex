83fa1f871bd227a7615054b45c20b4574984f37d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-01-16 21:25:47 +0100

    Refactor FeedUpdater

--- lib/crawler/updater.ex
+++ lib/crawler/updater.ex
@@ -2,14 +2,19 @@ defmodule ExRss.Crawler.Updater do
   require Logger
 
   alias ExRss.FeedUpdater
+  alias ExRss.Repo
   alias HTTPoison.Response
 
   def update(feed) do
     Logger.debug "Requesting feed from #{feed.url}"
 
     with {:ok, %Response{body: body}} <- HTTPoison.get(feed.url),
-         {:ok, raw_feed, _} <- FeederEx.parse(body) do
-      FeedUpdater.update(feed, raw_feed)
+         {:ok, raw_feed, _} <- FeederEx.parse(body),
+         {:ok, %{feed: new_feed}} <- FeedUpdater.update(feed, raw_feed) |> Repo.transaction
+      do
+      Logger.debug "Updated feed #{feed.title}"
+
+      {:ok, new_feed}
     else
       _ -> {:error, feed}
     end
--- web/models/feed_updater.ex
+++ web/models/feed_updater.ex
@@ -2,26 +2,21 @@ defmodule ExRss.FeedUpdater do
   require Ecto.Query
   require Logger
 
+  alias Ecto.Changeset
   alias Ecto.Multi
   alias ExRss.Entry
-  alias ExRss.Feed
-  alias ExRss.Repo
 
   def update(feed, raw_feed) do
     new_entries =
       raw_feed.entries
       |> Enum.map(fn entry -> Map.put(Entry.parse(entry), :feed_id, feed.id) end)
 
+    changeset =
+      feed
+      |> Changeset.change(%{url: raw_feed.url, title: raw_feed.title, updated_at: DateTime.utc_now})
+
     Multi.new
     |> Multi.insert_all(:insert_entries, Entry, new_entries, on_conflict: :nothing)
-    |> Multi.update_all(
-      :update_feed,
-      Ecto.Query.from(f in Feed, where: f.id == ^feed.id),
-      set: [url: raw_feed.url, title: raw_feed.title, updated_at: DateTime.utc_now])
-    |> Repo.transaction
-
-    Logger.debug "Updated feed #{feed.title}"
-
-    {:ok, feed}
+    |> Multi.update(:feed, changeset)
   end
 end
