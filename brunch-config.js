0ef31b477824b7260fa105687678cd4d1882cf5d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-02-08 17:25:58 +0100

    Depend on Bootstrap 4.0.0-alpha.6

--- brunch-config.js
+++ brunch-config.js
@@ -22,7 +22,7 @@ exports.config = {
     stylesheets: {
       joinTo: "css/app.css",
       order: {
-        after: ["web/static/css/app.css"] // concat app.css last
+        after: ["web/static/css/app.scss"]
       }
     },
     templates: {
@@ -54,6 +54,12 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/bootstrap/scss"],
+        precision: 8 // minimum precision required by bootstrap
+      }
     }
   },
 
