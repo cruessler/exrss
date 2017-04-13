e185e54659114f453483f29e5d38d0589fe3c44c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-04-13 15:54:21 +0200

    Add discover_feeds

--- web/models/feed_adder.ex
+++ web/models/feed_adder.ex
@@ -1,4 +1,16 @@
 defmodule ExRss.FeedAdder do
+  def discover_feeds(url) do
+    with {:ok, response} <- HTTPoison.get(url, [], follow_redirect: true),
+         200 <- response.status_code,
+         html <- Floki.parse(response.body)
+    do
+      {:ok, extract_feeds(html)}
+    else
+      i when is_integer(i) -> {:error, :wrong_status_code}
+      x -> x
+    end
+  end
+
   def extract_feeds(html) do
     html
     |> Floki.find("link[rel=alternate][type='application/rss+xml']")
