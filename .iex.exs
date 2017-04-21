dc650012699f7d2a3d125549289f9cbe281e082c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-04-21 14:48:47 +0200

    Add Debug.delete_feeds!

--- .iex.exs
+++ .iex.exs
@@ -5,4 +5,9 @@ alias ExRss.{Feed, Entry}
 
 defmodule Debug do
   def sql_query(sql), do: Ecto.Adapters.SQL.query(Repo, sql)
+
+  def delete_feeds! do
+    Repo.delete_all(Entry)
+    Repo.delete_all(Feed)
+  end
 end
