1c001feac676827ba683f02277d4bd0a8218f112
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-04-21 15:22:42 +0200

    Don’t change url on update
    The url might not be present in a feed. In case it is, the change would
    most probably not e expected by users.

--- web/models/feed_updater.ex
+++ web/models/feed_updater.ex
@@ -2,9 +2,9 @@ defmodule ExRss.FeedUpdater do
   require Ecto.Query
   require Logger
 
-  alias Ecto.Changeset
   alias Ecto.Multi
   alias ExRss.Entry
+  alias ExRss.Feed
 
   def update(feed, raw_feed) do
     new_entries =
@@ -16,10 +16,7 @@ defmodule ExRss.FeedUpdater do
 
     changeset =
       feed
-      |> Changeset.change(
-        %{url: raw_feed.url,
-          title: raw_feed.title,
-          updated_at: DateTime.utc_now})
+      |> Feed.changeset(%{title: raw_feed.title})
 
     Multi.new
     |> Multi.insert_all(:insert_entries, Entry, new_entries, on_conflict: :nothing)
