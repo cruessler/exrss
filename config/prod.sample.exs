c5671e3e3ff6805f6bab07dabdbf6f675f826230
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-05-12 12:34:34 +0200

    Add example files for prod environment

--- config/prod.sample.exs
+++ config/prod.sample.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ex_rss, ExRss.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: {:system, "HOST"}, port: 80],
+  url: [host: "", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
---
+++ config/prod.secret.sample.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :ex_rss, ExRss.Endpoint,
+  secret_key_base: ""
+
+# Configure your database
+config :ex_rss, ExRss.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "",
+  password: "",
+  database: "",
+  pool_size: 20
