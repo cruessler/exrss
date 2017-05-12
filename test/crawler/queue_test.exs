dead5cbdb1d9683f6dc2b2c75e085909cb0f5230
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-05-12 12:14:54 +0200

    Add test

--- test/crawler/queue_test.exs
+++ test/crawler/queue_test.exs
@@ -17,6 +17,11 @@ defmodule ExRss.Crawler.QueueTest do
   setup do
     # Explicitly get a connection before each test
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+    # Allow the queue to access the tests’ db connection
+    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+
+    :ok
   end
 
   test "timeout" do
@@ -28,9 +33,7 @@ defmodule ExRss.Crawler.QueueTest do
     assert_in_delta Queue.timeout([%{next_update_at: soon}]), 15_000, 100
   end
 
-  test "sends message to updater" do
-    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
-
+  test "sends message without next_update_at to updater immediately" do
     {:ok, pid} =
       Queue.start_link(store: TestStore, updater: TestUpdater)
 
@@ -39,5 +42,18 @@ defmodule ExRss.Crawler.QueueTest do
     GenServer.cast pid, {:add_feed, feed}
 
     assert_receive :update
+  end
+
+  test "sends message to updater with delay" do
+    {:ok, pid} =
+      Queue.start_link(store: TestStore, updater: TestUpdater)
+
+    next_update_at = Timex.shift(DateTime.utc_now, milliseconds: 200)
+
+    feed = %{title: "Test feed", next_update_at: next_update_at, pid: self()}
+
+    GenServer.cast pid, {:add_feed, feed}
+
+    assert_receive :update, 200
   end
 end
