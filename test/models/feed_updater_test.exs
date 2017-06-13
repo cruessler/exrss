6ce6eecf21d3210b32a0b003dd05e24343c5b716
Christoph Rüßler <christoph.ruessler@mailbox.org>
2017-06-13 11:45:22 +0200

    Add test

---
+++ test/models/feed_updater_test.exs
@@ -0,0 +1,34 @@
+defmodule ExRss.FeedUpdaterTest do
+  use ExRss.ModelCase
+
+  alias Ecto.Multi
+  alias ExRss.Feed
+  alias ExRss.FeedUpdater
+
+  test "updates entries" do
+    feed =
+      %Feed{id: 1,
+        title: "This is a title",
+        url: "http://example.com",
+        user_id: 1}
+
+    raw_feed =
+      %{title: "This is a title",
+        entries:
+          [%{title: "This is a title",
+            link: "http://example.com",
+            updated: DateTime.utc_now |> DateTime.to_string}
+          ]
+        }
+
+    multi = FeedUpdater.update(feed, raw_feed)
+
+    assert [
+      {:insert_entries, {:insert_all, ExRss.Entry, [entry], _}},
+      {:feed, {:update, feed_changeset, []}}
+    ] = Multi.to_list(multi)
+
+    assert entry.feed_id == feed.id
+    assert feed_changeset.valid?
+  end
+end
