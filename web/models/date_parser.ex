4f5a904c5daa2c1ebebffa348f06f3b86e6dd14f
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-03-27 18:14:03 +0200

    Add DateParser
    DateParser helps with inspecting publication dates and provides
    functions for parsing them in case they could not be parsed by previous
    versions of ExRss.

---
+++ web/models/date_parser.ex
@@ -0,0 +1,37 @@
+defmodule ExRss.DateParser do
+  require Logger
+
+  import Ecto.Query
+
+  alias Ecto.Changeset
+  alias ExRss.Entry
+  alias ExRss.Repo
+
+  def list_unparsed(number) do
+    from(e in Entry, where: is_nil(e.posted_at) and not is_nil(e.raw_posted_at), limit: ^number)
+    |> Repo.all()
+  end
+
+  def clear_parsed() do
+    Entry
+    |> Repo.update_all(set: [posted_at: nil])
+  end
+
+  def parse() do
+    from(e in Entry, where: is_nil(e.posted_at) and not is_nil(e.raw_posted_at))
+    |> Repo.all()
+    |> Enum.reduce({0, 0}, fn entry, {successes, errors} ->
+      case Entry.parse_time(entry.raw_posted_at) do
+        {:ok, parsed_time} ->
+          entry
+          |> Changeset.change(posted_at: parsed_time)
+          |> Repo.update!()
+
+          {successes + 1, errors}
+
+        {:error, _} ->
+          {successes, errors + 1}
+      end
+    end)
+  end
+end
