0dd397d53534307e99c1bef4600c11316c7bae0d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-05-14 17:18:15 +0200

    Wrap FeederEx.parse in try clause
    Feeder, the Erlang library FeederEx is a wrapper for, relies on `throw`
    to communicate a few error conditions. Before this commit, they would
    not be caught which could lead to ExRss going down.

--- lib/crawler/updater.ex
+++ lib/crawler/updater.ex
@@ -7,11 +7,26 @@ defmodule ExRss.Crawler.Updater do
 
   def update(feed) do
     with {:ok, %Response{body: body}} <- HTTPoison.get(feed.url),
-         {:ok, raw_feed, _} <- FeederEx.parse(body),
+         {:ok, raw_feed} <- parse_feed(body),
          {:ok, %{feed: new_feed}} <- FeedUpdater.update(feed, raw_feed) |> Repo.transaction() do
       {:ok, new_feed}
     else
       _ -> {:error, feed}
+    end
+  end
+
+  def parse_feed(xml) do
+    try do
+      FeederEx.parse(xml)
+    catch
+      :throw, value ->
+        {:error, value}
+    else
+      {:ok, raw_feed, _} ->
+        {:ok, raw_feed}
+
+      {:fatal_error, _, error, _, _} ->
+        {:error, error}
     end
   end
 end
