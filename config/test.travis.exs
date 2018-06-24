641c5eb85fee4a3efdd0342d81757db3c7dc1a66
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-06-24 18:15:38 +0200

    Configure PostgreSQL for Travis

---
+++ config/test.travis.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ex_rss, ExRss.Endpoint,
+  http: [port: 4001],
+  server: false,
+  secret_key_base: String.duplicate("a", 64)
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :ex_rss, ExRss.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "travis_ci_test",
+  pool: Ecto.Adapters.SQL.Sandbox
