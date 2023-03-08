07af364d8c1e86f31da94f8c10354f1327686845
Christoph Rüßler <christoph.ruessler@mailbox.org>
2023-03-08 20:10:58 +0100

    Update phoenix to 1.7.0
    Follow [upgrade-to-1.7].
    
    This update is incomplete in the sense that there are quite a few files
    that were generated by previous versions of Phoenix that have not been
    updated to the state they would be in were they to be generated today.
    
    [upgrade-to-1.7]: https://gist.github.com/chrismccord/00a6ea2a96bc57df0cce526bd20af8a7

--- lib/ex_rss_web.ex
+++ lib/ex_rss_web.ex
@@ -16,6 +16,11 @@ defmodule ExRssWeb do
   below.
   """
 
+  # `exrss` has not been set up to use verified routes yet. To change that,
+  # follow [the guide][1].
+  #
+  # [1]: https://gist.github.com/chrismccord/00a6ea2a96bc57df0cce526bd20af8a7
+
   def model do
     quote do
       use Ecto.Schema
