673ba8041d6215ef20c78ed3463a3ecc485fda7c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2023-10-29 11:27:50 +0100

    Update build.mjs to work with esbuild > 0.16

--- assets/build.mjs
+++ assets/build.mjs
@@ -1,13 +1,3 @@
-// 2023-04-07
-//
-// This file does not work with esbuild > 0.16 as esbuild’s API has changed in
-// 0.17.
-//
-// esbuild-sass-plugin is pinned to version < 2.6 because versions > 2.5 require
-// esbuild 0.17.
-//
-// https://github.com/evanw/esbuild/blob/main/CHANGELOG.md#0170
-
 import esbuild from 'esbuild';
 import ElmPlugin from 'esbuild-plugin-elm';
 import { sassPlugin } from 'esbuild-sass-plugin';
@@ -35,14 +25,6 @@ let opts = {
   plugins,
 };
 
-if (watch) {
-  opts = {
-    ...opts,
-    watch,
-    sourcemap: 'inline',
-  };
-}
-
 if (deploy) {
   opts = {
     ...opts,
@@ -50,14 +32,19 @@ if (deploy) {
   };
 }
 
-const promise = esbuild.build(opts);
-
 if (watch) {
-  promise.then((_result) => {
-    process.stdin.on('close', () => {
-      process.exit(0);
+  opts = {
+    ...opts,
+    sourcemap: 'inline',
+  };
+  esbuild
+    .context(opts)
+    .then((ctx) => {
+      ctx.watch();
+    })
+    .catch((_error) => {
+      process.exit(1);
     });
-
-    process.stdin.resume();
-  });
+} else {
+  esbuild.build(opts);
 }
