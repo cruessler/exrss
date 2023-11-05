8af00bb2b6c2e56344ca8efa582116e8bf126b04
Christoph Rüßler <christoph.ruessler@mailbox.org>
2023-11-05 11:45:02 +0100

    Replace usages of get_flash

--- lib/ex_rss_web.ex
+++ lib/ex_rss_web.ex
@@ -48,7 +48,7 @@ defmodule ExRssWeb do
       use Phoenix.View, root: "lib/ex_rss_web/templates", namespace: ExRssWeb
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_csrf_token: 0, view_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
