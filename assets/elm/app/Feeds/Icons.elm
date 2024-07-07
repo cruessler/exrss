4b188bd77b262083f61b98b9a49af245a8ca9227
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-07-07 20:48:48 +0200

    Use icons for entry actions

--- assets/elm/app/Feeds/Icons.elm
+++ assets/elm/app/Feeds/Icons.elm
@@ -1,4 +1,4 @@
-module Feeds.Icons exposing (bookmarkSlashSolid)
+module Feeds.Icons exposing (arrowTopRightOnSquareSolid, bookmarkSlashSolid)
 
 {-| This module contains icons that come from
 <https://jasonliang.js.org/heroicons-for-elm/>, but are not available in
@@ -10,6 +10,18 @@ installed by `elm install`.
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+
+
+arrowTopRightOnSquareSolid : Html msg
+arrowTopRightOnSquareSolid =
+    svg [ fill "none", viewBox "0 0 24 24", strokeWidth "1.5", stroke "currentColor" ]
+        [ Svg.path
+            [ strokeLinecap "round"
+            , strokeLinejoin "round"
+            , d "M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            ]
+            []
+        ]
 
 
 bookmarkSlashSolid : Html msg
