a1d93b3cf41e9e0e35d05e77a1a0e61dc1c23280
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-12-13 19:46:02 +0100

    Remove Elm code that has been replaced

--- assets/elm/app/Feeds/Options.elm
+++ assets/elm/app/Feeds/Options.elm
@@ -12,22 +12,6 @@ import Html.Events as E
 import Types.Feed as Feed exposing (Feed)
 
 
-radio : msg -> a -> a -> String -> Html msg
-radio onClick currentValue value text =
-    H.div []
-        [ H.label
-            []
-            [ H.input
-                [ A.type_ "radio"
-                , A.checked (value == currentValue)
-                , E.onClick onClick
-                ]
-                []
-            , H.text text
-            ]
-        ]
-
-
 collapsible : Bool -> List (Html Msg) -> Html Msg
 collapsible show children =
     if show then
@@ -35,77 +19,6 @@ collapsible show children =
 
     else
         H.div [] []
-
-
-actionsFieldset : Html Msg
-actionsFieldset =
-    H.fieldset [ A.class "border-2 p-2" ]
-        [ H.legend [] [ H.text "Actions" ]
-        , H.button
-            [ A.class "px-4 py-2 text-sm font-extrabold bg-blue-700 text-white"
-            , A.type_ "button"
-            , E.onClick GetFeeds
-            ]
-            [ H.text "Update feeds" ]
-        ]
-
-
-visibilityRadio : Visibility -> Visibility -> String -> Html Msg
-visibilityRadio currentVisibility visibility text =
-    radio (SetVisibility visibility) currentVisibility visibility text
-
-
-visibilityFieldset : Visibility -> Html Msg
-visibilityFieldset visibility =
-    H.fieldset [ A.class "border-2 p-4" ]
-        [ H.legend [] [ H.text "Visibility" ]
-        , visibilityRadio visibility
-            ShowAllEntries
-            "Show all entries"
-        , visibilityRadio visibility
-            HideReadEntries
-            "Hide read entries"
-        , visibilityRadio visibility
-            AlwaysShowUnreadEntries
-            "Smart: Show unread entries if feed is collapsed, show all entries if feed is not collapsed"
-        ]
-
-
-filterByRadio : FilterBy -> FilterBy -> String -> Html Msg
-filterByRadio currentFilterBy filterBy text =
-    radio (SetFilterBy filterBy) currentFilterBy filterBy text
-
-
-filterByFieldset : FilterBy -> Html Msg
-filterByFieldset filterBy =
-    H.fieldset [ A.class "border-2 p-4" ]
-        [ H.legend [] [ H.text "Filter feeds by" ]
-        , filterByRadio filterBy
-            DontFilter
-            "Don’t filter feeds"
-        , filterByRadio filterBy
-            FilterByErrorStatus
-            "Only show feeds with error"
-        ]
-
-
-sortByRadio : SortBy -> SortBy -> String -> Html Msg
-sortByRadio currentSortBy sortBy text =
-    radio (SetSortBy sortBy) currentSortBy sortBy text
-
-
-sortByFieldset : SortBy -> Html Msg
-sortByFieldset sortBy =
-    H.fieldset [ A.class "border-2 p-4" ]
-        [ H.legend [] [ H.text "Sort feeds by" ]
-        , sortByRadio sortBy
-            SortByNewest
-            "Sort by newest entry"
-        , sortByRadio sortBy
-            SortByNewestUnread
-            "Sort by newest unread entry"
-        , H.p [] [ H.text "Feeds with unread entries will always be shown first" ]
-        ]
 
 
 requests : Dict String Model.Request -> Html Msg
@@ -210,11 +123,7 @@ view model =
         , additionalInfo model.feeds
         , collapsible
             model.showOptions
-            [ actionsFieldset
-            , visibilityFieldset model.visibility
-            , filterByFieldset model.filterBy
-            , sortByFieldset model.sortBy
-            , Discovery.discoverFeedsFieldset model.discoveryUrl
+            [ Discovery.discoverFeedsFieldset model.discoveryUrl
             , Discovery.discoverFeedFieldset
             , requests model.requests
             ]
