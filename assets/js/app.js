4f99dad506245c89878784e5f84c636603d7a5fa
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-12-19 11:56:39 +0100

    Don't load Elm apps in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,10 @@
-import NewFeed from '../elm/app/App/NewFeed.elm';
-
 import { Socket } from '../../deps/phoenix';
 import { LiveSocket } from '../../deps/phoenix_live_view';
-
-const hooks = {
-  ElmModules: {
-    mounted() {
-      mountElmModules();
-    },
-  },
-};
 
 const csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content');
 const liveSocket = new LiveSocket('/live', Socket, {
-  hooks,
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
 });
@@ -27,32 +16,6 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
-
-const Elm = {
-  App: { NewFeed: NewFeed.Elm.App.NewFeed },
-};
-
-const mountElmModules = () => {
-  const nodes = document.querySelectorAll('[data-elm-module]');
-
-  return [...nodes].reduce((acc, node) => {
-    // The module name may be `App` or `App.Feeds`. `modulePath` would be
-    // ["App"] or ["App", "Feeds"], then.
-    const moduleName = node.dataset.elmModule;
-    const modulePath = moduleName.split('.');
-    const elmModule = modulePath.reduce((acc, part) => acc[part], Elm);
-
-    const params = JSON.parse(node.dataset.elmParams) || {};
-
-    if (elmModule != undefined) {
-      acc[moduleName] = elmModule.init({ node, flags: params });
-    } else {
-      console.error(`No module named ‘${moduleName}’ could be found`);
-    }
-
-    return acc;
-  }, {});
-};
 
 window.addEventListener('phx:live_reload:attached', ({ detail: reloader }) => {
   reloader.enableServerLogs();
