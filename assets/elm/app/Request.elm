11982b473b078eca162665fda51a7930e97c375b
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-12-24 16:28:46 +0100

    Remove remaining Elm code

--- assets/elm/app/Request.elm
+++
@@ -1,6 +0,0 @@
-module Request exposing (Request(..))
-
-
-type Request a b c
-    = InProgress a
-    | Done (Result b c)
--- lib/ex_rss_web/views/app_view.ex
+++ lib/ex_rss_web/views/app_view.ex
@@ -1,5 +1,4 @@
 defmodule ExRssWeb.AppView do
-  import Phoenix.HTML
   use PhoenixHTMLHelpers
 
   def form_group(form, field, attrs, fun) when is_function(fun, 1) do
@@ -11,17 +10,5 @@ defmodule ExRssWeb.AppView do
     end
 
     content_tag(:div, fun.(form), class: class)
-  end
-
-  def elm_module(module, params \\ %{}, attrs \\ []) do
-    {tag, attrs} = Keyword.pop(attrs, :tag, :div)
-
-    data_attributes = [
-      "data-elm-module": module,
-      "data-elm-params": html_escape(Jason.encode!(params)),
-      "phx-hook": "ElmModules"
-    ]
-
-    content_tag(tag, "", Keyword.merge(attrs, data_attributes))
   end
 end
