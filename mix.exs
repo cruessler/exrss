0b4ba14f65ebafa4aa29468d4202c73ce5665db2
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-01-10 14:15:20 +0100

    Update minimum Elixir version to 1.17
    Update Erlang to 27.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExRss.Mixfile do
     [
       app: :ex_rss,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
