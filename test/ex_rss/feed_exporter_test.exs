b0cb1348ba0e1db783ef9ace2b825535950f5cc7
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-09-18 21:45:22 +0200

    Add FeedExporter

--- test/ex_rss/feed_exporter_test.exs
+++ test/ex_rss/feed_exporter_test.exs
@@ -4,11 +4,26 @@ defmodule ExRss.FeedExporterTest do
   import ExRss.AccountsFixtures
   import ExRss.FeedFixtures
 
+  alias ExRss.FeedExporter
+
   test "exports feed" do
     %{id: user_id} = user_fixture()
     feed = feed_fixture(%{user_id: user_id})
 
     assert %{} = feed
     assert length(feed.entries) == 6
+
+    opml = FeedExporter.export_feeds([feed])
+
+    assert opml ==
+             String.trim("""
+             <?xml version="1.0" encoding="utf-8"?>\
+             <opml version="1.0">\
+             <head/>\
+             <body>\
+             <outline type="rss" text="some content" xmlUrl="https://example.com/some-content"/>\
+             </body>\
+             </opml>
+             """)
   end
 end
